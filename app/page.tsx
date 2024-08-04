4cbf79d3b571ca2d3d8775e76a0c52653ff32d9a
outclassstudio <mlimshs@gmail.com>
2024-08-04 23:57:25 +0900

    Test: Tailwind CSS(2)

--- app/page.tsx
+++ app/page.tsx
@@ -1,37 +1,30 @@
 export default function Home() {
   return (
-    <main className="bg-gray-100 h-screen flex items-center justify-center p-5 dark:bg-gray-700">
-      <div className="bg-white shadow-lg p-5 w-full max-w-screen-sm rounded-3xl dark:bg-gray-500">
-        <div className="flex justify-between items-center">
-          <div className="flex flex-col">
-            <span className="text-gray-600 font-semibold -mb-1 dark:text-gray-300">
-              In transit
-            </span>
-            <span className="text-4xl font-bold dark:text-white">Coolblue</span>
-          </div>
-          <div className="size-12 rounded-full bg-orange-500"></div>
-        </div>
-        <div className="flex items-center my-2 gap-2">
-          <span
-            className="bg-green-400 rounded-3xl px-5 py-2 text-xl text-white font-semibold
-             transition hover:bg-green-500 hover:cursor-pointer hover:scale-125"
-          >
-            TODAY
-          </span>
-          <span className="text-2xl font-semibold dark:text-gray-100">
-            9:30-10:30u
-          </span>
-        </div>
-        <div className="relative">
-          <div className="bg-gray-200 w-full h-2 rounded-full absolute"></div>
-          <div className="bg-green-500 w-2/3 h-2 rounded-full absolute"></div>
-        </div>
-        <div className="flex justify-between items-center mt-5 text-gray-600 text-xl dark:text-gray-100">
-          <span>Expected</span>
-          <span>Sorting center</span>
-          <span>In transit</span>
-          <span className="text-gray-400 dark:text-gray-500">Delivered</span>
-        </div>
+    <main
+      className="bg-gray-100 h-screen flex items-center justify-center p-5 dark:bg-gray-700
+    sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100"
+    >
+      <div
+        className="bg-white shadow-lg p-5 w-full max-w-screen-sm rounded-3xl dark:bg-gray-500
+      flex flex-col md:flex-row gap-2"
+      >
+        <input
+          className="w-full rounded-full h-12 bg-gray-200 pl-5 outline-none
+          ring ring-transparent focus:ring-green-500 focus:ring-offset-2 transition-shadow
+          placeholder:drop-shadow invalid:focus:ring-red-500 peer"
+          type="email"
+          required
+          placeholder="Email address"
+        />
+        <span className="text-red-400 font-medium hidden peer-invalid:block">
+          Email is required
+        </span>
+        <button
+          className="text-white py-2 rounded-full active:scale-90 outline-none transition-transform 
+          font-medium md:px-10 bg-black peer-invalid:bg-orange-500"
+        >
+          Login
+        </button>
       </div>
     </main>
   );
