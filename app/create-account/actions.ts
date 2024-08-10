d86a7e33207ad9c4c6440884c1073cb9112dae30
outclassstudio <mlimshs@gmail.com>
2024-08-10 22:21:24 +0900

    Feat: Authentication(1)

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -4,7 +4,12 @@ import {
   PASSWORD_REGEX,
   PASSWORD_REGEX_ERROR,
 } from "@/lib/constants";
+import db from "@/lib/db";
 import { z } from "zod";
+import bcrypt from "bcrypt";
+import { getIronSession } from "iron-session";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
 const passwordRegex = new RegExp(PASSWORD_REGEX);
 
@@ -20,6 +25,30 @@ const checkPassword = ({
   confirm_password: string;
 }) => password === confirm_password;
 
+const checkUniqueUsername = async (username: string) => {
+  const user = await db.user.findUnique({
+    where: {
+      username,
+    },
+    select: {
+      id: true,
+    },
+  });
+  return !Boolean(user);
+};
+
+const checkUniqueEmail = async (email: string) => {
+  const user = await db.user.findUnique({
+    where: {
+      email,
+    },
+    select: {
+      id: true,
+    },
+  });
+  return !Boolean(user);
+};
+
 const formSchema = z
   .object({
     username: z
@@ -31,16 +60,17 @@ const formSchema = z
       .max(10, "너무 길어요")
       .trim()
       .toLowerCase()
-      .refine(checkUsername, "potato는 안돼요"),
-    email: z.string().email().trim(),
-    password: z
+      .refine(checkUsername, "potato는 안돼요")
+      .refine(checkUniqueUsername, "이미 존재하는 이름이에요"),
+    email: z
       .string()
-      .min(PASSWORD_MIN_LENGTH)
-      .regex(passwordRegex, PASSWORD_REGEX_ERROR),
-    confirm_password: z
-      .string()
-      .min(PASSWORD_MIN_LENGTH)
-      .regex(passwordRegex, PASSWORD_REGEX_ERROR),
+      .email()
+      .trim()
+      .refine(checkUniqueEmail, "이미 존재하는 이메일이에요"),
+    password: z.string().min(PASSWORD_MIN_LENGTH),
+    // .regex(passwordRegex, PASSWORD_REGEX_ERROR),
+    confirm_password: z.string().min(PASSWORD_MIN_LENGTH),
+    // .regex(passwordRegex, PASSWORD_REGEX_ERROR),
   })
   .refine(checkPassword, {
     message: "비밀번호가 일치하지 않아요",
@@ -56,10 +86,28 @@ export const createAccount = async (prevState: any, formData: FormData) => {
     confirm_password: formData.get("confirm_password"),
   };
 
-  const result = formSchema.safeParse(data);
+  const result = await formSchema.spa(data);
   if (!result.success) {
     return result.error.flatten();
   } else {
-    console.log(result);
+    const hashedPassword = await bcrypt.hash(result.data.password, 12);
+    const user = await db.user.create({
+      data: {
+        username: result.data.username,
+        email: result.data.email,
+        password: hashedPassword,
+      },
+      select: {
+        id: true,
+      },
+    });
+    const cookie = await getIronSession(cookies(), {
+      cookieName: "new-cookie",
+      password: process.env.COOKIE_PASSWORD!,
+    });
+    //@ts-ignore
+    cookie.id = user.id;
+    await cookie.save();
+    redirect("/profile");
   }
 };
---
+++ app/profile/page.tsx
@@ -0,0 +1,3 @@
+export default function Profile() {
+  return <div>login success!!!</div>;
+}
