335f7d2284dabc87766681fa918dcb8cfeb1d3bc
outclassstudio <mlimshs@gmail.com>
2024-08-18 17:41:51 +0900

    Feat: Product(1)

---
+++ app/(tabs)/products/page.tsx
@@ -0,0 +1,27 @@
+import ListProduct from "@/components/list-product";
+import db from "@/lib/db";
+
+async function getProducts() {
+  const products = await db.product.findMany({
+    select: {
+      id: true,
+      title: true,
+      price: true,
+      created_at: true,
+      photo: true,
+    },
+  });
+  return products;
+}
+
+export default async function Products() {
+  const products = await getProducts();
+
+  return (
+    <div className="p-5 flex flex-col gap-5">
+      {products.map((product) => (
+        <ListProduct key={product.id} {...product} />
+      ))}
+    </div>
+  );
+}
