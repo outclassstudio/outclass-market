f152894f58916a72b71aab513fbc8e5ddc3c877b
outclassstudio <mlimshs@gmail.com>
2024-08-23 22:06:33 +0900

    Feat: Set cache(1)

--- app/(tabs)/home/page.tsx
+++ app/(tabs)/home/page.tsx
@@ -2,9 +2,18 @@ import ProductList from "@/components/product-list";
 import db from "@/lib/db";
 import { PlusIcon } from "@heroicons/react/24/solid";
 import { Prisma } from "@prisma/client";
+import { unstable_cache as nextCache, revalidatePath } from "next/cache";
 import Link from "next/link";
 
+//cache 사용 -> 함수는 return이 반드시 있어야 함
+const getCashedProducts = nextCache(getInitialProducts, ["home-products"]);
+// const revalidate = async () => {
+//   "use server";
+//   revalidatePath("/home");
+// };
+
 async function getInitialProducts() {
+  console.log("hit");
   const products = await db.product.findMany({
     select: {
       id: true,
@@ -13,7 +22,7 @@ async function getInitialProducts() {
       created_at: true,
       photo: true,
     },
-    take: 1,
+    // take: 1,
     orderBy: {
       created_at: "desc",
     },
@@ -25,12 +34,21 @@ export type InitialProducts = Prisma.PromiseReturnType<
   typeof getInitialProducts
 >;
 
+export const metadata = {
+  title: "home",
+};
+
+export const revalidate = 60;
+
 export default async function Products() {
   const initialProducts = await getInitialProducts();
 
   return (
     <div>
       <ProductList initialProducts={initialProducts} />
+      {/* <form action={revalidate}>
+        <button>Revalidate</button>
+      </form> */}
       <Link
         href="/products/add"
         className="bg-orange-500 flex items-center justify-center 
