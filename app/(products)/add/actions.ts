35350e1cfebf74d0a0a1632dafdf810f51123eee
outclassstudio <mlimshs@gmail.com>
2024-09-05 12:47:01 +0900

    Feat: Set cloudflare image upload

--- app/(products)/add/actions.ts
+++ app/(products)/add/actions.ts
@@ -16,11 +16,11 @@ export async function uploadProduct(_: any, formData: FormData) {
   };
 
   //! 이미지 파일을 로컬에 저장
-  if (data.photo instanceof File) {
-    const photoData = await data.photo.arrayBuffer();
-    await fs.appendFile(`./public/${data.photo.name}`, Buffer.from(photoData));
-    data.photo = `/${data.photo.name}`;
-  }
+  // if (data.photo instanceof File) {
+  //   const photoData = await data.photo.arrayBuffer();
+  //   await fs.appendFile(`./public/${data.photo.name}`, Buffer.from(photoData));
+  //   data.photo = `/${data.photo.name}`;
+  // }
 
   //todo schema의 폴더를 변경할 필요가 있음
   const result = productSchema.safeParse(data);
@@ -51,4 +51,18 @@ export async function uploadProduct(_: any, formData: FormData) {
       redirect(`/products/${product.id}`);
     }
   }
+}
+
+export async function getUploadUrl() {
+  const response = await fetch(
+    `https://api.cloudflare.com/client/v4/accounts/${process.env.IMAGE_ACCOUNT}/images/v2/direct_upload`,
+    {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${process.env.IMAGE_TOKEN}`,
+      },
+    }
+  );
+  const data = await response.json();
+  return data;
 }
