5b57a9c5b5cf138d3fd39b76131ee9a81d743d0e
outclassstudio <mlimshs@gmail.com>
2024-09-12 12:29:54 +0900

    Feat: Edit Post(1)

---
+++ app/posts/edit/[id]/page.tsx
@@ -0,0 +1,27 @@
+import PostEditForm from "@/components/life/post-edit-form";
+import db from "@/lib/db";
+import { Prisma } from "@prisma/client";
+
+async function getPost(id: number) {
+  const post = await db.post.findUnique({
+    where: {
+      id,
+    },
+    select: {
+      title: true,
+      description: true,
+    },
+  });
+  return post;
+}
+
+export type InitialPostType = Prisma.PromiseReturnType<typeof getPost>;
+
+export default async function EditPost({
+  params: { id },
+}: {
+  params: { id: string };
+}) {
+  const post = await getPost(+id);
+  return <PostEditForm initialPost={post} />;
+}
